cff8ad2cbfa6de6215b497d4dbab3463a1b79ffd
felixerus <felixerus@gmail.com>
2018-01-18 01:42:02 -0800

    Move from recursive search for max to updating an internal max stack.

--- src/main/java/MaxStack/MaxStack.java
+++ src/main/java/MaxStack/MaxStack.java
@@ -5,47 +5,43 @@ import java.util.Stack;
 // MaxStack uses generics extending Comparable since it will need to compare elements
 public class MaxStack<E extends Comparable> extends Stack<E> {
 
+    private Stack<E> max_stack; // internal stack to track current max
 
-    // my initial instinct was to go with recursion to avoid data duplication
+    public MaxStack() {
+        max_stack = new Stack<E>();
+    }
+
+    public E pop() {
+        max_stack.pop();
+        return super.pop();
+    }
+
+    // switch from recursion in getMax to pushing the current max onto the max_stack
+    public E push(E item) {
+        if (max_stack.isEmpty()) {
+            max_stack.push(item);
+        }
+        else {
+            E max_element = max_stack.peek();
+            if (item.compareTo(max_element) > 0) {
+                max_stack.push(item);
+            }
+            else {
+                max_stack.push(max_element);
+            }
+        }
+
+        return super.push(item);
+    }
+
+    // can return null
+    // switching from recursion, we go from O(n) to O(1)
     public E getMax() {
-
-        E current_element;
-        E max_element;
 
         if (isEmpty()) {
             return null;
         }
 
-        else {
-            current_element = pop(); // get the top element
-            max_element = getMax(); // recursive call to get max element from remainder of the stack
-
-            if (max_element == null) {
-                return current_element;
-            }
-
-            if (current_element.compareTo(max_element) > 0) {
-                max_element = current_element; // compare current element to max found from recursive call
-            }
-
-            push(current_element); // calling push after the recursion will restore the stack.
-        }
-
-        return max_element;
-    }
-
-    // simple test
-    public static void main(String[] args) {
-        MaxStack<Integer> myStack = new MaxStack<Integer>();
-        for (int i = 0; i < 10; i++) {
-            myStack.push(i);
-        }
-
-        Integer max_element = myStack.getMax();
-        System.out.println("Max element is: " + max_element);
-
-        while (!myStack.isEmpty()) {
-            System.out.println(myStack.pop());
-        }
+        return max_stack.peek();
     }
 }
